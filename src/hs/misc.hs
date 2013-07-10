745e42b69dae8b9f52da258d3a26abc980bd4025
Chris Heller <hellertime@gmail.com>
2013-07-10 00:02:48 -0400

    working out the depth first state... argh

--- src/hs/misc.hs
+++ src/hs/misc.hs
@@ -1,3 +1,5 @@
+import Data.Set (Set)
+
 import Graph 
 import Graph.Edge
 import Graph.Node
@@ -9,3 +11,22 @@ graph1 = foldl addEdge emptyGraph $ concat $ map toEdge edgeList
   where
     edgeList = [('a', "bc"), ('b', "d"), ('c', "de"), ('e', "a")]
     toEdge = \(s, ts) -> map (Edge (Node s) . Node) ts
+
+data DFWState a = DFW { pre_i   :: Int
+                      , rpost_j :: Int
+                      , pre     :: [(Node a, Int)]
+                      , rpost   :: [(Node a, Int)]
+                      , visited :: Set (Node a)
+                      , succs   :: [Node a]
+                      , forest  :: Graph a
+                      } deriving (Show)
+
+dfw :: DFWState a -> Node a -> DFWState a
+dfw s n =
+  where
+    visited' = Set.insert n $ visited s
+    go x s@(_, _, _, _, v, d) y = if Set.member y v
+                                      then s
+                                      else
+                                          
+    (i + 1, j' - 1, (n,i):pre'', (n,j'):rpost'', visited'', d'')
